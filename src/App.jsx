4c688c7a7695b9fdaa649b0f0dc8a057370e063b
chetan0910 <chetan.prf55@gmail.com>
2025-05-16 23:35:58 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">App</h1>
+    </>
+  );
+}
+
+export default App;
